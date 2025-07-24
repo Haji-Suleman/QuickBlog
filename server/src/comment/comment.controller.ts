9b6268a5a1d09602c8a7052a67ac0ff2bf4294b1
Haji Suleman <mirhajisulemanjamali@gmail.com>
2025-07-25 00:00:55 +0500

    pushing comments section

--- server/src/comment/comment.controller.ts
+++ server/src/comment/comment.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { CommentService } from './comment.service';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Controller('comment')
 export class CommentController {
-    constructor(private readonly commentService: CommentService){}
+    constructor(private readonly commentService: CommentService) {
+    }
+    @Post("/add-comment")
+    async addComment(@Body() body) {
+        return this.commentService.addComment(body)
+    }
+    @Post("/comments")
+    async getApprovedBlogs(@Body() body) {
+        return this.commentService.getBlogsComment(body)
+    }
 }
--- server/src/comment/comment.service.ts
+++ server/src/comment/comment.service.ts
@@ -1,16 +1,47 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
+import { Document, Model } from 'mongoose';
 
 @Injectable()
 export class CommentService {
-    constructor(@InjectModel("comment") commentModel) {
+    constructor(@InjectModel("comment") private readonly commentModel: Model<Document>) {
     }
     async addComment(body) {
         try {
             const { blog, name, content } = body
+            await this.commentModel.create(blog, name, content)
+            return { success: true, message: "Comment  added for reviews" }
         } catch (error) {
             return { success: false, message: error.message }
         }
 
+    }
+    async getBlogsComment(body) {
+        try {
+            const { blogId } = body
+            const comments = await this.commentModel.find({ blog: blogId, isApproved: true }).sort({ createdAt: -1 })
+            return { success: true, comments }
+        } catch (error) {
+            return { success: false, message: error.message }
+        }
+    }
+    async getAllComment(body) {
+        try {
+            const comments = await this.commentModel.find({}).populate("blog").sort({ createdAt: -1 })
+            return { success: true, comments }
+        } catch (error) {
+            return { success: false, message: error.message }
+
+        }
+    }
+    async getDeashboard(body) {
+        try {
+            const recentBlogs = await this.commentModel.find({}).sort({ createdAt: -1 }).limit(5)
+            const blogs = await this.commentModel.countDocuments();
+            const 
+        }
+        catch (error) {
+            return { success: false, message: error.message }
+        }
     }
 }
